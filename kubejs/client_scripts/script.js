0b130ab710289a090d63c8d6be189fb59b634014
Benji <Benji07p@gmail.com>
2024-01-25 12:42:02 +0100

    Add tooltips for Players Ingots

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -14,4 +14,8 @@ onEvent('jei.information', event=> {
 onEvent('item.tooltip', tooltip => {
 	tooltip.add('timeextraminer:universal_reconstructor', ['May reconstructing the universe', 'Maybe not'])
 	tooltip.add('extendedcrafting:ultimate_singularity', ['Not consumed in alchemical ingot duplication'])
+	tooltip.add('timeextraminer:furingots', ['The Owner'])
+	tooltip.add('timeextraminer:benjingots', ['The Developper'])
+	tooltip.add('timeextraminer:alphmium_ingot', ['The Leader'])
+	tooltip.add('timeextraminer:doukingots', ['The Builder'])
 })
